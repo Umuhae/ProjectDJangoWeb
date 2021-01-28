0a0587d29d76da4ec0298e6d69b761425b438077
Umuhae <konlp@kakao.com>
2021-01-29 06:24:54 +0900

    [*] fix redirect

--- accountapp/views.py
+++ accountapp/views.py
@@ -18,7 +18,7 @@ has_ownership = [account_ownership_required, login_required]
 class CreateUserForm(CreateView):
     model = get_user_model()
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 
