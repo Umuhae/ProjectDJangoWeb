269f86255e3ac938a5d8a9b3320cff947aadea3f
Umuhae <konlp@kakao.com>
2021-01-29 05:11:36 +0900

    requirements.txt

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.9.0
+
+WORKDIR /home/
+
+RUN git clone https://github.com/Umuhae/ProjectDJangoWeb.git
+
+WORKDIR /home/pragmatic/
+
+RUN pip install -r requirements.txt
+
+RUN echo "SECRET_KEY=_6igx5=9*k0v)i1ph3#g4)8*3-n6a%$3g_*wdd2o0$%pygi0x4" > .env
+
+RUN python manage.py migrate
+
+EXPOSE 8000
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+
--- articleapp/models.py
+++ articleapp/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
 
     title = models.CharField(max_length=200, null=True)
-    image = models.ImageField(upload_to='article/', null=False)
+    image = models.ImageField(upload_to='article/', null=False, blank=True)
     content = models.TextField(null=True)
 
     created_at = models.DateField(auto_created=True, null=True)
