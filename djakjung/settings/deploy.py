05c6bbca9d0344b442ae6a533a530b9f4148e66b
Umuhae <konlp@kakao.com>
2021-01-30 05:17:14 +0900

    [*] secret key settings

--- djakjung/settings/deploy.py
+++ djakjung/settings/deploy.py
@@ -6,28 +6,11 @@ def read_secret(secret_name):
         secret = file.read().strip()
     return secret
 
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-# reading .env file
-
-
 ALLOWED_HOSTS = ['*']
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
