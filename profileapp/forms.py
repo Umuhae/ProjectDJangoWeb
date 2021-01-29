63004d0f87ef3a35a51373756758e77c40b91bad
Umuhae <konlp@kakao.com>
2021-01-30 06:13:00 +0900

    [*] 안내 멘트 정리

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -6,3 +6,8 @@ class ProfileCreationForm(ModelForm):
     class Meta:
         model = Profile
         fields = ["image", 'nickname', 'message']
+        labels = {
+            'image': '썸네일 변경',
+            'nickname': '닉네임 변경',
+            'message': '자기소개 변경',
+        }
--- projectapp/forms.py
+++ projectapp/forms.py
@@ -7,3 +7,8 @@ class ProjectCreateForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'image', 'description']
+        labels = {
+            'title': '게시판 제목',
+            'image': '게시판 대표이미지',
+            'description': '게시판 설명',
+        }
