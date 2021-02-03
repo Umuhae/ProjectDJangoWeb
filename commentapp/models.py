3e1d7d63eadaf4adc2ff064756ee27b54e74af67
Umuhae <konlp@kakao.com>
2021-02-04 02:37:23 +0900

    [*] change comment max length

--- commentapp/models.py
+++ commentapp/models.py
@@ -5,7 +5,7 @@ from articleapp.models import Article
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.SET_NULL, related_name='comment',null=True)
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='comment',null=True)
-    content = models.TextField()
+    content = models.TextField(max_length=5000)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
